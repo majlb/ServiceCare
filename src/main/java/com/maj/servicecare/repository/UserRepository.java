1f32e1c4960d0ba4eb4c154039d1e3ccd8a0762f
Mohamad Jarrah <mohamad.jarrah@sword-group.com>
2025-06-21 00:39:08 +0300

    Change for repository

--- src/main/java/com/maj/servicecare/repository/UserRepository.java
+++ src/main/java/com/maj/servicecare/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.maj.servicecare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.maj.servicecare.entity.User;
 
+@Repository 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
